cb95f4e31910b400fbf7cb153343b52903553ebe
Josh Arce <josh.arce@outlook.com>
2017-05-03 21:06:24 -0700

    checkpoint-3-routing: complete
    Create templates for album, collection and landing.
    Remove css animations from css files.
    Set up UI-Router to handle states and create routes for landing and album.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,23 @@
 (function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
 
-var app = angular.module('blocJams', []);
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            });
+    }
 
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
 })();
